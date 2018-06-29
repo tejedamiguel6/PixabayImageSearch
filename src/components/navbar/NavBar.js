72a25bfd2d7cd175eafd2f078b977b534b8370b8
tejedamiguel6 <tejedamiguel6@aol.com>
2018-06-29 10:07:45 -0400

    first commit

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+
+const NavBar = () => {
+  return (
+    <AppBar title="PixaBay Image Finder" />
+  )
+}
+
+export default NavBar;
